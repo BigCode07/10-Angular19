1b55f817c13f15edad6307a49989931dc0a4ce5e
bigcode <bigcode07@gmail.com>
2025-04-21 20:30:00 -0300

    feat: Configurar alias de importaciones en TypeScript

--- src/app/dashboard/dashbboard.component.ts
+++ src/app/dashboard/dashbboard.component.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SidemenuComponent } from '../shared/sidemenu/sidemenu.component';
+import { SidemenuComponent } from '@shared/sidemenu/sidemenu.component';
 
 @Component({
   selector: 'app-dashbboard',
