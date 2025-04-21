ce5a82eaf476f645c64a4e48e9631f11fe219fcd
bigcode <bigcode07@gmail.com>
2025-04-21 20:21:02 -0300

    feat: Sintaxis @for & @if

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,16 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { routes } from '../../app.routes';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'shared-sidemenu',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './sidemenu.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SidemenuComponent {
   public menuItems = routes
+    // Por cada ruta, toma su propiedad "children" si existe, si no, retorna un array vacío
     .map((route) => route.children ?? [])
+    // Aplana el array de arrays de rutas hijas en un único array
     .flat()
+    // Filtra para quedarse solo con las rutas que existen y tienen definido un "path"
     .filter((route) => route && route.path)
+    // Filtra las rutas que NO contienen ":" en su "path"
+    // (es decir, elimina rutas que contienen parámetros como ":id", ":nombre", etc.)
     .filter((route) => !route.path?.includes(':'));
 }
