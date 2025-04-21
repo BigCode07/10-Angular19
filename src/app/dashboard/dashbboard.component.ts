6897055377848ddc4e4136b7d0dc5540d354ee74
bigcode <bigcode07@gmail.com>
2025-04-21 19:59:59 -0300

    feat: Configuración del dashboard - Tailwind

--- src/app/dashboard/dashbboard.component.ts
+++ src/app/dashboard/dashbboard.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidemenuComponent } from '../shared/sidemenu/sidemenu.component';
 
 @Component({
   selector: 'app-dashbboard',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidemenuComponent],
   templateUrl: './dashbboard.component.html',
 })
 export default class DashbboardComponent {}
--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,9 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
-  selector: 'app-sidemenu',
+  selector: 'shared-sidemenu',
   imports: [],
   templateUrl: './sidemenu.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SidemenuComponent { }
+export class SidemenuComponent {
+  public menuItems = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route && route.path)
+    .filter((route) => !route.path?.includes(':'));
+}
